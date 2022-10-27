b3a8cd65d93ac2fd98a92310751c9c4454e94f02
Farish Noorhisham <farishnoorhisham81@gmail.com>
2022-10-27 18:48:41 +0100

    chartjs, csv parser

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FileUploader } from "react-drag-drop-files";
+import * as d3 from "d3"
+import { useEffect, useState } from 'react';
+import { DSVRowArray } from 'd3';
+import { Scatter } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
+
+const fileTypes = ["CSV"];
 
 function App() {
+
+  const [data, setData] = useState<DSVRowArray<string> | null>(null)
+  const [file, setFile] = useState<File | null>(null)
+  const [x, setX] = useState<string | null>(null)
+  const [y, setY] = useState<string | null>(null)
+
+  const handleChange = async (file: File) => {
+    console.log("File:")
+    setFile(file)
+    setData(d3.csvParse(await file.text()))
+  }
+
+  useEffect(() => {
+
+  }, [data])
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: y || ""
+        }
+      },
+      x: {
+        title: {
+          display: true,
+          text: x || ""
+        }
+      }
+    },
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <FileUploader handleChange={handleChange} types={fileTypes} />
+      <p>X axis</p>
+      {data?.columns.map((colName) => {
+    return (
+      <button onClick={() => setX(colName)}>{colName}</button>
+    )
+  })}
+    <p>Y axis</p>
+        {data?.columns.map((colName) => {
+    return (
+      <button onClick={() => setY(colName)}>{colName}</button>
+    )
+  })}
+      <Scatter style={{ maxWidth: 700, maxHeight: 700}} options={options} data={{datasets: [
+    {
+      label: file ? file.name :'A dataset',
+      data: data?.map((row) => ({
+        x: row[x || 0],
+        y: row[y || 0],
+      })),
+      backgroundColor: 'rgba(255, 99, 132, 1)',
+    },
+  ]}} />
     </div>
   );
 }
